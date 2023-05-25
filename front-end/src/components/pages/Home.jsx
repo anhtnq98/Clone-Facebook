cb2aeea5c7cbd178b4e421c01e5a0f44516b9b32
anhtnq98 <kethanlinh.otaku@gmail.com>
2023-05-25 08:39:14 +0700

    add

--- front-end/src/components/pages/Home.jsx
+++ front-end/src/components/pages/Home.jsx
@@ -4,7 +4,11 @@ import "./Home.css";
 function Home() {
   return (
     <div className="home-container">
-      <div className="home-left">tin trái</div>
+      <div className="home-left">
+        <div className="left-top"></div>
+        <div className="left-middle"></div>
+        <div className="left-footer"></div>
+      </div>
       <div className="home-middle">tin giữa</div>
       <div className="home-right">tin phải</div>
     </div>
